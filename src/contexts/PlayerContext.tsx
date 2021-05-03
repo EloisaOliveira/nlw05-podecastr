143c927bf47f24dc564cfe9cff68b8dacca1ea0c
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2021-05-03 02:12:57 -0300

    feat: shuffle control created

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -13,6 +13,7 @@ type PlayerContextData = {
     currentEpisodeIndex: number;
     isPlaying: boolean;
     isLooping: boolean;
+    isShuffling: boolean;
     play: (episode: Episode) => void;
     playList: (list: Episode[], index: number) => void;
     playNext: () => void;
@@ -20,6 +21,7 @@ type PlayerContextData = {
     setPlayingState: (state: boolean) => void;
     togglePlay: () => void;
     toggleLoop: () => void;
+    toggleShuffle: () => void;
     hasPrevious: boolean;
     hasNext: boolean;
 }
@@ -35,6 +37,7 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps){
     const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
     const [isPlaying, setIsPlaying] = useState(false);
     const [isLooping, setIsLooping] = useState(false);
+    const [isShuffling, setIsShuffling] = useState(false);
   
     function play(episode: Episode) {
       setEpisodeList([episode]);
@@ -55,6 +58,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps){
     function toggleLoop() {
       setIsLooping(!isLooping)
     }
+
+    function toggleShuffle(){
+      setIsShuffling(!isShuffling);
+    }
   
     function setPlayingState(state: boolean) {
       setIsPlaying(state)
@@ -64,7 +71,11 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps){
     const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
 
     function playNext(){
-      if (hasNext){
+      if (isShuffling) {
+        const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+
+        setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+      } else if (hasNext){
         setCurrentEpisodeIndex(currentEpisodeIndex + 1)
       }
     }
@@ -86,11 +97,13 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps){
           playPrevious,
           isPlaying,
           isLooping,
+          isShuffling,
           togglePlay,
           setPlayingState,
           hasNext,
           hasPrevious,
-          toggleLoop
+          toggleLoop,
+          toggleShuffle
         }}
       >
         {children}
